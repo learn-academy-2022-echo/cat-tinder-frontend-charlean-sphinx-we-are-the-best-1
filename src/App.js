bf0a686327d1a0c96f6aa02bded1b8b45efb1522
luislaurel <luislaurel133@gmail.com>
2022-09-19 18:20:47 -0600

    Everything working, on create mock story.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer'
+import Header from './components/Header'
+import CatIndex from './pages/CatIndex'
+import CatShow from './pages/CatShow'
+import CatNew from './pages/CatNew'
+import CatEdit from './pages/CatEdit'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import { Routes, Route } from "react-router-dom"
 
-function App() {
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/catindex" element={<CatIndex />} />
+      <Route path="/catshow" element={<CatShow />} />
+      <Route path="/catnew" element={<CatNew />} />
+      <Route path="/catedit" element={<CatEdit />} />
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
+    <Footer />
+  </>
+)
 }
 
 export default App;
